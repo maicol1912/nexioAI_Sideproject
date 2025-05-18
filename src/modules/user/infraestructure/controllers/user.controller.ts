c4b69d9a3960fb02f0dd21fa91f991d3fe68f4eb
maicol1912 <maicolarcila34@gmail.com>
2025-05-18 18:03:23 -0500

    feat: change ThrottlerGuard

--- src/modules/user/infraestructure/controllers/user.controller.ts
+++ src/modules/user/infraestructure/controllers/user.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, HttpCode, HttpStatus, Inject, Param, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Inject, Param, ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { UserUsecasesProxyModule } from '../usecase-proxy/user.usecase.proxy.module';
 import type { UseCaseProxy } from '@shared/domain/types/usecases-proxy';
 import type { UserUseCases } from '../../usecases/user.usecases';
 import { UserDto } from './dtos/user.dto';
+import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 
 @Controller('user')
 export class UserController {
@@ -20,5 +21,19 @@ export class UserController {
   @Get(':id')
   public findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
     return UserDto.fromModel(this.userUseCaseProxy.getInstance().findOne(id));
+  }
+
+  @UseGuards(ThrottlerGuard)
+  @Throttle({ default: { limit: 3, ttl: 60000 } })
+  @Get('gretting/hola')
+  @HttpCode(HttpStatus.OK)
+  public gretting(){
+    return 'hello'
+  }
+
+  @Get('gretting/hola2')
+  @HttpCode(HttpStatus.OK)
+  public gretting2(){
+    return 'hello'
   }
 }
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { BcryptService } from './infraestructure/services/bcrypt/bcrypt.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtTokenService } from './infraestructure/services/jwt/jwt.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
     imports: [
@@ -9,9 +11,20 @@ import { JwtTokenService } from './infraestructure/services/jwt/jwt.service';
             secret: process.env.JWT_SECRET,
             signOptions: { expiresIn: '24h' },
         }),
+        ThrottlerModule.forRoot({
+            throttlers: [
+                {
+                    ttl: 60000,
+                    limit: 10,
+                },
+            ],
+        }),
     ],
     controllers: [],
-    providers: [BcryptService, JwtTokenService],
+    providers: [
+        BcryptService,
+        JwtTokenService
+    ],
     exports: [BcryptService, JwtTokenService]
 })
 export class SharedModule { }
